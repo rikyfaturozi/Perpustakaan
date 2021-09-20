763f81f96e5a677b2a6ff04e5cd234cd52e1497d
rikyfaturozi <rikyfatur@gmamil.com>
2021-09-20 23:27:17 +0700

    Upload Latihan1

--- application/controllers/Contoh_1.php
+++ application/controllers/Contoh_1.php
@@ -2,9 +2,11 @@
 class Contoh_1 extends CI_Controller
 {
     public function index ()
-    {
-        echo "Hallo Saya Riky Faturozi";
-        echo "Saya hoby Berolahraga";
-        echo "Saya seorang mahasiswa UBSI PEMUDA";
+    {  
+        echo "<h1> Perkenalkakn <br></h1>";
+        echo " Hallo Saya Riky Faturozi <br>" ;   
+        echo "Saya Hoby Berolahraga <br>";
+        echo "Saya Seorang Mahasiswa UBSI PEMUDA <br>";
+        echo "Saya Umur 20 Tahun";
     }
 }
